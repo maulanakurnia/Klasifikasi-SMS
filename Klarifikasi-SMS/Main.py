1e0cff58655e355a0c0344515115d4d354053d70
maulanaakurniaa@yahoo.com <maulanaakurniaa@yahoo.com>
2020-05-14 21:25:33 +0800

    Perubahan

--- Klarifikasi-SMS/Main.py
+++ Klarifikasi-SMS/Main.py
@@ -3,20 +3,14 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 import sys, os
 
 import string
-import re
 import json
-from collections import Counter
+import numpy as np
+import pandas as pd
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans
-
-import numpy as np
-import pandas as pd
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from tqdm import tqdm
-
 
 class MyWindow(QMainWindow):
     def __init__(self,a,b,c,d,e):
@@ -35,36 +29,20 @@ class MyWindow(QMainWindow):
         k = self.inputk.text()
         textsms = self.textsms.toPlainText()
 
-
         modelknn = KNeighborsClassifier(n_neighbors=int(k), weights='distance')
         modelknn.fit(self.x_train,self.y_train)
 
-        kalimat_tes = textsms
-        kalimat_tes = kalimat_tes.split(", ")
+        sms = textsms
+        sms = sms.split(",, ")
 
-
-
-        # membersihkan dokumen pengujian
-        kalimat_tes_bersih = self.clean_with_loop(kalimat_tes)
-
-        # definisikan nama label
         nama_label = ["normal", "penipuan", "penawaran"]
 
-
-        # loop untuk prediksi kelompok
-        for teks in kalimat_tes_bersih:
+        for teks in sms:
             arr_teks = []
             arr_teks.append(teks)
             vektor = self.vectorizer.transform(arr_teks)
             prediksi_label_knn = modelknn.predict(vektor)
-            # self.hasil.setText("kelompok: " + nama_label[np.int(prediksi_label_knn)]+ "\n")
-            QMessageBox.about(self, "Hasil Klarifikasi", "Kelompok : "+nama_label[np.int(prediksi_label_knn)]+"\n")
-            # print(teks, ":\n" + "- kelompok: " + nama_label[np.int(prediksi_label_knn)]+ "\n")
-
-
-
-        # print(textsms)
-        # print(shost)
+            QMessageBox.about(self, "Hasil Klarifikasi","\n " + "Kelompok : "+nama_label[np.int(prediksi_label_knn)]+"\t\n\t")
 
     def initUI(self):
         self.setGeometry(2000, 200, 600, 300)
@@ -92,70 +70,22 @@ class MyWindow(QMainWindow):
         self.b1.setGeometry(130, 175,200,25)
         self.b1.clicked.connect(self.button_clicked)
 
-    def clean(self, doc):
-        stop_free = " ".join([i for i in doc.lower().split() if i not in self.stopword])
-        punc_free = ''.join(ch for ch in stop_free if ch not in self.punctuation)
-        stemmer = StemmerFactory().create_stemmer()
-        normalized = stemmer.stem(punc_free)
-        processed = re.sub(r"\d+","",normalized)
-        y = processed.split()
-        return y
-
-    def clean_with_loop(self, arr):
-        print("Memuat Data")
-        hasil = []
-        progress = tqdm(arr)
-        for item in progress:
-            # progress.set_description("Membersihkan dokumen")
-            cleaned = self.clean(item)
-            cleaned = ' '.join(cleaned)
-            hasil.append(cleaned)
-        return hasil
-
-
-def clean(doc,stopword,punctuation):
-    stop_free = " ".join([i for i in doc.lower().split() if i not in stopword])
-    punc_free = ''.join(ch for ch in stop_free if ch not in punctuation)
-    stemmer = StemmerFactory().create_stemmer()
-    normalized = stemmer.stem(punc_free)
-    processed = re.sub(r"\d+","",normalized)
-    y = processed.split()
-    return y
-
-def clean_with_loop(arr,b,c):
-    print("Memuat Data")
-    hasil = []
-    progress = tqdm(arr)
-    for item in progress:
-        # progress.set_description("Membersihkan dokumen")
-        cleaned = clean(item,b,c)
-        cleaned = ' '.join(cleaned)
-        hasil.append(cleaned)
-    return hasil
-
-
 
 def window():
     app = QApplication(sys.argv)
-
 
     data_stopword = json.load(open('stopwords-id.json','r'))
     stopword = set(data_stopword)
     punctuation = set(string.punctuation)
 
-    # ini object
-    # win = MyWindow()
-
     sms_csv = pd.read_csv('dataset_sms.csv')
-    sms = []
+    sms_bersih = []
     for index, row in sms_csv.iterrows():
-        sms.append(row["Teks"])
+        sms_bersih.append(row["Teks"])
 
     y_train = []
     for index, row in sms_csv.iterrows():
         y_train.append(row["label"])
-
-    sms_bersih = clean_with_loop(sms,stopword,punctuation)
 
     vectorizer = TfidfVectorizer(stop_words=data_stopword)
     x_train = vectorizer.fit_transform(sms_bersih)
